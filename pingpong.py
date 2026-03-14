9ffa754977bf9648fce22de77675dce7723d0ace
NizaOS <nizanurani46@gmail.com>
2026-03-15 01:04:10 +0530

    added a few more things, added a reelative path finder so that anyone can run the script with the images showing up

--- pingpong.py
+++ pingpong.py
@@ -1,4 +1,10 @@
-import pygame, sys, random
+import pygame, sys, random, os
+
+
+def load_asset(filename):
+    """Finds the path to an asset relative to this script's location."""
+    current_path = os.path.dirname(__file__)
+    return os.path.join(current_path, filename)
 
 def ball_animation():
     global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
@@ -9,15 +15,13 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
 
-    if ball.left <=0:
+    if ball.left <= 0:
         player_score += 1
         score_time = pygame.time.get_ticks()
 
-        
     if ball.right >= screen_width:
         opponent_score += 1
         score_time = pygame.time.get_ticks()
-
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1   
@@ -38,7 +42,6 @@ def oppononet_ai():
             opponent.y -= opponent_speed
         else:
             opponent.y += opponent_speed
-  
 
     if opponent.top <= 0:
         opponent.top = 0
@@ -71,31 +74,45 @@ def ball_restart():
         score_time = None
         return None
 
+
 pygame.init()
 clock = pygame.time.Clock()
 
 screen_width = 1280
 screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('pingpong')
+pygame.display.set_caption('Winter Ping-Pong')
 
-iceblock = pygame.image.load("/Users/MyPC/brick.png").convert_alpha()
-iceblock_l = pygame.transform.scale(iceblock, (15, 210))
-iceblock_r = pygame.transform.flip(iceblock_l, True, False)
 
-background = pygame.image.load('/Users/MyPC/Downloads/vecteezy_winter-snowfall-at-midnight-with-the-moon-and-stars-on-sky_16157330_175/vecteezy_winter-snowfall-at-midnight-with-the-moon-and-stars-on-sky_16157330.jpg')
+try:
+    
+    iceblock_img = pygame.image.load(load_asset("brick.png")).convert_alpha()
+    iceblock_l = pygame.transform.scale(iceblock_img, (15, 210))
+    iceblock_r = pygame.transform.flip(iceblock_l, True, False)
+
+    background = pygame.image.load(load_asset("background.jpg")).convert()
+    background = pygame.transform.scale(background, (screen_width, screen_height))
+except FileNotFoundError:
+    print("Error: Could not find image assets in the script folder!")
+    print("Make sure 'brick.png' and 'background.jpg' are in the same directory as this script.")
+    pygame.quit()
+    sys.exit()
+
 
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 105, 15, 210)
 opponent = pygame.Rect(10, screen_height/2 - 105, 15, 210)
 
-bg_color =pygame.Color('grey12')
+
+bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
+
 
 ball_speed_x = 7 * random.choice((1,-1))
 ball_speed_y = 7 * random.choice((1,-1))
 player_speed = 0
 opponent_speed = 7
+
 
 player_score = 0
 opponent_score = 0
@@ -112,27 +129,29 @@ while True:
             if event.key == pygame.K_DOWN:
                 player_speed += 7
             if event.key == pygame.K_UP:
-                player_speed -=7
+                player_speed -= 7
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
                 player_speed -= 7
             if event.key == pygame.K_UP:
-                player_speed +=7
+                player_speed += 7
 
+    
     ball_animation()
     player_animation()
     oppononet_ai()
     
-   
-          
+    
     screen.fill(bg_color)
     screen.blit(background, (0,0))
+    
     if score_time:
         timer_number = ball_restart()
         if timer_number:
             timer_surf = timer_font.render(timer_number, True, light_grey)
-            timer_rect = timer_surf.get_rect(center=(screen_width/2,  screen_height/2 -50))
+            timer_rect = timer_surf.get_rect(center=(screen_width/2,  screen_height/2 - 50))
             screen.blit(timer_surf, timer_rect)
+            
     screen.blit(iceblock_l, player)
     screen.blit(iceblock_r, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
@@ -142,5 +161,6 @@ while True:
     screen.blit(player_text, (660, 470))
     opponent_text = score_font.render(f"{opponent_score}", False, light_grey)
     screen.blit(opponent_text, (605, 470))
+    
     pygame.display.flip()
     clock.tick(60)
